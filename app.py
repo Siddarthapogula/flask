466be9088fc09a7015dc35e389f1b53fbdddfc72
Siddartha pogula <pogulaSiddharth96@gmail.com>
2024-09-05 12:27:49 +0530

    add user signup

--- app.py
+++ app.py
@@ -18,7 +18,22 @@ def login_user(userName, password):
     else :
         return 'user is not a valid user'
 
-@app.route('/signup')
+@app.post('/signup')
 def signup():
-    
+    if request.method == 'GET':
+        return 'please use POST method'
+    data = request.get_json()
+    return validata_signup(data)
+
+def validata_signup(data):
+    username = data['username']
+    password = data['password']
+    mobileno = data['mobileno']
+    print(username.isalnum(), password.isalnum(), len(mobileno))
+    if len(username) <3 or len(username) > 32 or not username.isalnum() or len(password)<8 or len(password) > 32 or not password.isalnum() or len(mobileno) != 10:
+        return 'invalid user'
+    else :
+        return 'valid user & user successfully registered'
+
+
 app.run(debug = True)
